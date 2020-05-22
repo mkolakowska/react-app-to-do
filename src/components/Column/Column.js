f6861a24c0b2f4d236702beb83db6d8bd179d2bf
Malgorzata Kolakowska <m.a.kolakowska@googlemail.com>
2020-05-22 10:22:15 +0100

    Add columns and cards

--- src/components/Column/Column.js
+++ src/components/Column/Column.js
@@ -1,19 +1,25 @@
 import React from 'react';
 import styles from './Column.scss';
 import PropTypes from 'prop-types';
-// import Creator from '../Creator/Creator.js';
+import Creator from '../Creator/Creator.js';
 import Card from '../Card/Card.js';
+import { settings } from '../../data/dataStore';
 import Icon from '../Icon/Icon.js';
 
 class Column extends React.Component {
   static propTypes = {
-    title: PropTypes.node.isRequired,
+    title: PropTypes.node,
     cards: PropTypes.array,
     icon: PropTypes.node,
+    addCard: PropTypes.func,
+  };
+
+  static defaultProps = {
+    icon: settings.defaultColumnIcon,
   };
 
   render() {
-    const { title, icon, cards } = this.props;
+    const { title, icon, cards, addCard } = this.props;
     return (
       <section className={styles.component}>
         <h3 className={styles.title}>
@@ -22,20 +28,14 @@ class Column extends React.Component {
             <Icon name={icon} />
           </span>
         </h3>
-
         <div className={styles.cards}>
           {cards.map((cardData) => (
             <Card key={cardData.id} {...cardData} />
           ))}
         </div>
-        {/*
         <div className={styles.creator}>
-          <Creator
-            text={settings.cardCreatorText}
-            action={(title) => this.addCard(title)}
-          />
+          <Creator text={settings.cardCreatorText} action={addCard} />
         </div>
-        */}
       </section>
     );
   }
--- src/components/List/List.js
+++ src/components/List/List.js
@@ -5,7 +5,7 @@ import PropTypes from 'prop-types';
 import Column from '../Column/ColumnContainer';
 import { settings } from '../../data/dataStore';
 import ReactHtmlParser from 'react-html-parser';
-// import Creator from '../Creator/Creator.js';
+import Creator from '../Creator/Creator.js';
 
 class List extends React.Component {
   static propTypes = {
@@ -13,6 +13,7 @@ class List extends React.Component {
     description: PropTypes.node,
     columns: PropTypes.array,
     image: PropTypes.string.isRequired,
+    addColumn: PropTypes.func,
   };
 
   static defaultProps = {
@@ -20,25 +21,19 @@ class List extends React.Component {
   };
 
   render() {
-    const { title, image, description, columns } = this.props;
+    const { title, image, description, columns, addColumn } = this.props;
     return (
       <section className={styles.component}>
         <Hero titleText={title} image={image} />
         <div className={styles.description}>{ReactHtmlParser(description)}</div>
-
         <div className={styles.columns}>
           {columns.map((columnData) => (
             <Column key={columnData.id} {...columnData} />
           ))}
         </div>
-        {/*
         <div className={styles.creator}>
-          <Creator
-            text={settings.columnCreatorText}
-            action={(title) => this.addColumn(title)}
-          />
+          <Creator text={settings.columnCreatorText} action={addColumn} />
         </div>
-          */}
       </section>
     );
   }
